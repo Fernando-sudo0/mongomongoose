a9f6bf260337aec7b961c60dad707b3eb2b1eb8b
Fernando Gutierrez <fernando.gtz.escarrega@gmail.com>
2022-06-25 18:57:56 -0700

    people by name

--- myApp.js
+++ myApp.js
@@ -42,7 +42,7 @@ const createManyPeople = (arrayOfPeople, done) => {
     })
 
 };
-
+ 
 const findPeopleByName = (personName, done) => {
   Person.find({name : personName}, function(err, data){
       if(err) return console.log(err);
